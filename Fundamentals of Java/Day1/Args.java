4b5ebcf490720d0b5425774b356836c875fd5e26
Gokul-S-dev <sggokul762@gmail.com>
2026-05-18 14:12:02 +0530

    hello world

---
+++ Fundamentals of Java/Day1/Args.java
@@ -0,0 +1,9 @@
+public class Args {
+    public static void main( String[] args) {
+        System.out.println("The list of arguments passed");
+        for (String arg : args){
+            System.out.println(arg);
+        }
+    }
+    
+}
