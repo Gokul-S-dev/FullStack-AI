c452d67fd8d2c12b68e44479099d4f495db143c9
Gokul-S-dev <sggokul762@gmail.com>
2026-05-19 18:52:44 +0530

    Day 1 of frontend

---
+++ Frontend/Day1/compontent/src/Course.jsx
@@ -0,0 +1,14 @@
+const course1 = "HTML";
+
+function Course() {
+    return (
+        <>
+            <img src="" alt=""/>
+            <h2>{course1}</h2>
+            <p>This is an introductory course to {course1}.</p>   
+
+        </>
+    )
+}
+
+export default Course;
---
+++ Frontend/Day1/compontent/src/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+function Navbar() {
+    return (
+        <nav>
+            <h2>Code io</h2>
+            <button>Login</button>
+        </nav>
+    );
+}
+
+export default Navbar;
